9b3150b7b251d438155b7888bb76787ef13de5cd
Kadyr Sultan <sssultan.k@gmail.com>
2024-01-30 09:59:09 +0600

    09:59

---
+++ TSIS3/Function_Part1/1.py
@@ -0,0 +1,4 @@
+grams = float(input())
+def ounces_translate(x):
+    return 28.3495231 * x
+print(ounces_translate(grams))
---
+++ TSIS3/Function_Part1/2.py
@@ -0,0 +1,4 @@
+temp_Fahrenheit = float((input()))
+def Fahrenheit_temperature(x):
+    return (5/9) * (x -32) #Convert to centigrade temperature
+print(Fahrenheit_temperature(temp_Fahrenheit))
