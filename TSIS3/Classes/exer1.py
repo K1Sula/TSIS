422007d4eb1f9391bfab77f1f7ecab5a3cc5d699
Kadyr Sultan <sssultan.k@gmail.com>
2024-02-03 20:52:04 +0600

    tsis3 task 1 about classes

---
+++ TSIS3/Classes/exer1.py
@@ -0,0 +1,14 @@
+class Changesrt:
+    def __init__(self):
+        self.str1 = ""
+        
+    def getString(ssl): 
+        ssl.str1 = str(input())
+        
+    def printString(self):
+        print("This is your new string: ", self.str1.upper())
+        
+newstr = Changesrt()
+
+newstr.getString()
+newstr.printString()
